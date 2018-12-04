bb2b1d06ca60198eef9282685a8ec7fe8f82018b
ha-matsumu <ha.matsumu@gmail.com>
2018-12-04 17:46:23 +0900

    updated main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 //   - 先頭から「1, 2, 3, 4, 5」の値を持つ配列を作る
 //   - 配列を保存する変数の名前は「array1」とする
 //   - 配列を作成したらconsole.logで配列の内容を出力する
-let array1 = [1, 2, 3, 4, 5];
+const array1 = [1, 2, 3, 4, 5];
 console.log(array1);
 
 
@@ -26,7 +26,7 @@ console.log(array1);
 //     - 1番目の要素が「4, 5, 6」
 //     - 2番目の要素が「7, 8, 9」
 //   - 2次元配列を作成したらconsole.logで配列の内容を出力する
-let array2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const array2 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
 console.log(array2);
 
 
@@ -46,7 +46,7 @@ console.log(array2[1][0]);
 //   - 配列の末尾に追加する値は「'コメント4'」とする
 //   - 配列の末尾に値を追加する際には、配列の組み込みメソッドである「push」を使う
 //   - 値を配列の末尾に追加する前と追加した後の結果をconsole.logで出力して、'コメント4'が追加されていることを確認する
-let array3 = ["コメント1", "コメント2", "コメント3"];
+const array3 = ["コメント1", "コメント2", "コメント3"];
 console.log("末尾に値を追加する前: ", array3);
 array3.push("コメント4");
 console.log("末尾に値を追加した後: ", array3);
@@ -59,9 +59,9 @@ console.log("末尾に値を追加した後: ", array3);
 //   - 「pop」を使って取り除いた値は変数「poppedValue」に代入する
 //   - 値を配列の末尾から取り除く前と取り除いた後の結果をconsole.logで出力する
 //   - 変数「poppedValue」をconsole.logで出力して、'タスク3'が表示されることを確認する
-let array4 = ["タスク1", "タスク2", "タスク3"];
+const array4 = ["タスク1", "タスク2", "タスク3"];
 console.log("末尾から値を取り除く前: ", array4);
-let poppedValue = array4.pop();
+const poppedValue = array4.pop();
 console.log("末尾から値を取り除いた後: ", array4);
 console.log("poppedValueは「タスク3」であるはず: ", poppedValue);
 
@@ -72,7 +72,7 @@ console.log("poppedValueは「タスク3」であるはず: ", poppedValue);
 //   - 配列の先頭に追加する値は「'最優先タスク'」とする
 //   - 配列の先頭に値を追加する際には、配列の組み込みメソッドである「unshift」を使う
 //   - 値を配列の先頭に追加する前と追加した後の結果をconsole.logで出力して、'最優先タスク' が追加されていることを確認する
-let array5 = ["タスク1", "タスク2", "タスク3"];
+const array5 = ["タスク1", "タスク2", "タスク3"];
 console.log("先頭に値を追加する前: ", array5);
 array5.unshift("最優先タスク");
 console.log("先頭に値を追加した後: ", array5);
@@ -85,8 +85,8 @@ console.log("先頭に値を追加した後: ", array5);
 //   - 「shift」を使って取り除いた値は変数「shiftedValue」に代入する
 //   - 値を配列の先頭から取り除く前と取り除いた後の結果をconsole.logで出力する (出力結果が異なることを確認する)
 //   - 変数「shiftedValue」をconsole.logで出力して、'タスク1' が表示されることを確認する
-let array6 = ["タスク1", "タスク2", "タスク3"];
+const array6 = ["タスク1", "タスク2", "タスク3"];
 console.log("先頭から値を取り除く前: ", array6);
-let shiftedValue = array6.shift();
+const shiftedValue = array6.shift();
 console.log("先頭から値を取り除いた後: ", array6);
 console.log("shiftedValueは「タスク1」であるはず: ", shiftedValue);
